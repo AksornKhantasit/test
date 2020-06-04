05fb161ddbf0f6f3c3134f44c3f5ff31c10dc400
AKSORN KHANTASIT <aksorn.khantasit@scb.co.th>
2020-06-04 14:07:19 +0700

    Initial Commit

---
+++ searchBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  searchBar
+//
+//  Created by AKSORN KHANTASIT on 4/6/2563 BE.
+//  Copyright © 2563 AKSORN KHANTASIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
